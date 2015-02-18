62f051e1483ae8af58a5544ac8d10daeda1e1f54
Personnettaz Clement <adm-2w22a-06.univ-avignon.fr>
2015-02-18 17:22:37 +0100

    Création du fichier main pour les tests.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*
+---------Auteur---------
+~Clément Personnettaz
+
+Date de création : 18/02/2015
+
+Date de mise a Jour : 18/02/2015
+*/
+#include <iostream>
+#include "matricecreuse.h"
+#include "operationsimples.h"
+#include "multiplication.h"
+#include "transposee.h"
+#include "interfacegraphique.h"
+#include "iomatrice.h"
+
+using namespace std;
+
+int main()
+{
+
+}
