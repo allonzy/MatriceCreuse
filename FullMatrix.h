3b037a85676d3ec9db0659a3887eff378cae3374
Vivier Simon <adm-2w22a-06.univ-avignon.fr>
2015-03-18 16:14:04 +0100

    Ajout d'un squelette pour FullMatrix.h

---
+++ FullMatrix.h
@@ -0,0 +1,17 @@
+#ifndef _FULLMATRIX_
+#define _FULLMATRIX_
+#include <iostream>
+#include <map>
+using namespace std;
+class FullMatrix
+{
+private:
+	int** fullMatrix_m;
+	int fullMatrix_height;
+	int fullMatrix_width;
+public:
+	FullMatrix();
+	~FullMatrix();
+		
+};
+#endif
