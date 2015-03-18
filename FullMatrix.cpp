829ed6ff5b3f68e902693c129e645cb9cfa145b8
Marliere Jean <adm-2w22a-06.univ-avignon.fr>
2015-03-18 18:05:27 +0100

    Amélioration des fichiers FullMatrix

--- FullMatrix.cpp
+++ FullMatrix.cpp
@@ -1,17 +1,59 @@
-#ifndef _FULLMATRIX_
-#define _FULLMATRIX_
 #include <iostream>
-#include <map>
+#include "FullMatrix.h"
+
 using namespace std;
-class FullMatrix
-{
-private:
-	int** fullMatrix_m;
-	int fullMatrix_height;
-	int fullMatrix_width;
-public:
-	FullMatrix();
-	~FullMatrix();
+
+FullMatrix::FullMatrix(){}
+
+FullMatrix::FullMatrix(int height, int width){
+	FullMatrix_setHeight(height);
+	FullMatrix_setWidth(width);
+}
+
+FullMatrix::FullMatrix(string path){
+	ifstream dataMatrix(path);
+	if (!dataMatrix.isOpen()){
+		cerr << "FullMatrix error : invalid path.\n";
+		return;
+	}
+	else{
 		
-};
-#endif
+	}
+
+}
+
+FullMatrix::~FullMatrix(){
+	
+}
+
+int FullMatrix::FullMatrix_getHeight(){return fullMatrix_height;}
+
+int FullMatrix::FullMatrix_getWidth(){return fullMatrix_width;}
+
+int FullMatrix::FullMatrix_getValue(int x, int y){
+	if (x > FullMatrix_getHeight() || y > FullMatrix_getWidth()){
+		cerr << "getValue error : value is out of range.\n";
+		return 0;
+	}
+	else return fullMatrix_m[x][y];
+}
+
+void FullMatrix::FullMatrix_setHeight(int value){fullMatrix_height = value;}
+
+void FullMatrix::FullMatrix_setWidth(int value){fullMatrix_width = value;}
+
+void FullMatrix::FullMatrix_setValue(int x, int y, int value){
+	if (x > FullMatrix_getHeight() || y > FullMatrix_getWidth()){
+		cerr << "setValue error : value is out of range.\n";
+		return;
+	}
+	else {fullMatrix_m[x][y] = value;}
+
+void FullMatrix::FullMatrix_display(){
+	for (int x = 0; x < FullMatrix_getHeight(); x++){
+		for (int y = 0; y < FullMatrix_getWidth(); y++){
+			cout << FullMatrix_getValue(x, y) << " ";
+		}
+		cout << endl;
+	}
+}
--- FullMatrix.h
+++ FullMatrix.h
@@ -2,16 +2,29 @@
 #define _FULLMATRIX_
 #include <iostream>
 #include <map>
+
 using namespace std;
+
 class FullMatrix
 {
-private:
-	int** fullMatrix_m;
-	int fullMatrix_height;
-	int fullMatrix_width;
-public:
-	FullMatrix();
-	~FullMatrix();
-		
+	private:
+		int** fullMatrix_m;
+		int fullMatrix_height;
+		int fullMatrix_width;
+	public:
+		FullMatrix();
+		FullMatrix(int height, int width)
+		FullMatrix(string path);
+		~FullMatrix();
+
+		int FullMatrix_getHeight();
+		int FullMatrix_getWidth();
+		int FullMatrix_getValue(int x, int y);
+
+		void FullMatrix_setHeight(int value);
+		void FullMatrix_setWidth(int value);
+		void FullMatrix_setValue(int x, int y, int value);
+
+		void FullMatrix_display();
 };
 #endif
